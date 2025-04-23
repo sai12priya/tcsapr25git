ded394cd86853027755df5ccc09a7610d4ededcc
sai12priya <147846920+sai12priya@users.noreply.github.com>
2025-04-23 10:05:25 +0000

    Update testcases.java
    testcase 2 added

--- testcases.java
+++ testcases.java
@@ -3,6 +3,10 @@ class Testcases{
   public void testMethod(){
      assert(false);
   }
+  @Test
+  public void testMethod2(){
+     assert(true);
+  }
 @Test
   public void testMethod3(){
      assert(true);
