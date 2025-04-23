51f874d3030ce94adf49936c458c391e8132a013
sai12priya <saipriyankasathelli@gmail.com>
2025-04-23 10:09:43 +0000

    added four method

--- testcases.java
+++ testcases.java
@@ -4,7 +4,20 @@ class Testcases{
      assert(false);
   }
 @Test
+  public void testMethod2(){
+     assert(true);
+  }
+@Test
   public void testMethod3(){
      assert(true);
   }
+@Test
+  public void testMethod4(){
+     assert(true);
+  }
+@Test
+  public void testMethod5(){
+     assert(true);
+  }
+
 }
